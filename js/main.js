f0091889336f291abe2d63e58d8733fa7b05e01c
iamrt <rance@ranceturley.com>
2015-10-28 21:02:05 -0500

    2nd Mock-up finished

--- js/main.js
+++ js/main.js
@@ -26,8 +26,42 @@ $(function () {
 		$p.css('fill', $svgFill)
 	});
 
+/*********************************************************/
 
+/****************** Logo Responsiveness ******************/
+
+	winWidth = window.innerWidth;
+	
+
+	if (winWidth < 768) {
+		$('#logo').addClass('img-responsive');
+	}	else {
+		$('#logo').removeClass('img-responsive');
+	};
 
 /*********************************************************/
 
+/********************** Mobile Menu **********************/
+
+	$('#mobile-menu').on('click', function() {
+		$('nav ul').slideToggle(600)
+		$('button').toggleClass('change-bg')
+		$('.icon-bar').toggleClass('change-icon');
+	});
+
+/*********************************************************/
+
+
+/******************* Footer to Bottom ********************/
+	
+	$docHeight = $(document).height();
+	$winHeight = $(window).height();
+
+	if ($docHeight > $winHeight) {
+		$('.wrapper').css('min-height', $docHeight);
+	}	else {
+		$('.wrapper').css('min-height', $winHeight);
+	};
+
+/*********************************************************/
 });
